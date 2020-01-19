4d1fada3411163f6d7976f3c7178181736d20585
George <georgesamuel96>
2020-01-19 15:33:21 +0200

    Add navigation menu and navigation view

--- app/src/main/java/com/example/georgesamuel/kotlinapp/ui/home/HomeActivity.kt
+++ app/src/main/java/com/example/georgesamuel/kotlinapp/ui/home/HomeActivity.kt
@@ -2,12 +2,27 @@ package com.example.georgesamuel.kotlinapp.ui.home
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
 import com.example.georgesamuel.kotlinapp.R
+import kotlinx.android.synthetic.main.activity_home.*
 
 class HomeActivity : AppCompatActivity() {
+
+    lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        setSupportActionBar(toolbar)
+        navController = Navigation.findNavController(this, R.id.fragment)
+        NavigationUI.setupWithNavController(nav_view, navController)
+        NavigationUI.setupActionBarWithNavController(this, navController, drawer_layout)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return NavigationUI.navigateUp(navController, drawer_layout)
     }
 }
